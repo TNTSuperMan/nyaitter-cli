6cfe111868963fd4c094a46f27bc3beeac7263ea
TNTSuperMan <111977067+TNTSuperMan@users.noreply.github.com>
2026-09-17 13:55:57 +0900

    ルーターを入れる

--- src/App.tsx
+++ src/App.tsx
@@ -1,52 +1,26 @@
 import { Box, Text } from "ink";
-import { Suspense, use, useEffect, useState } from "react";
-import { client, realtime } from "./client";
-import type { User } from "./client/types";
-import { SimpleUser } from "./view/SimpleUser";
-import { Button } from "./view/Button";
+import { createContext, Suspense, use, useEffect, useState } from "react";
+import { Home } from "./pages/Home";
 
-const me_promise = client.getMe();
+export const NavigatorContext = createContext<(path: keyof typeof routes) => void>(() => { });
 
-function AppAsync() {
-  const me = use(me_promise).user as User;
-  const [notify_unreads, setNotifyUnreads] = useState(me.notification_unread_count);
-
-  useEffect(() => {
-    realtime.on("notificationUnreadCount", setNotifyUnreads);
-    return () => {
-      realtime.off("notificationUnreadCount", setNotifyUnreads);
-    };
-  }, []);
-
-  return (
-    <Box>
-      <Box marginRight={1}>
-        <Text bold italic>nyaitter-cli</Text>
-        <Button char="h" onClick={() => { }} />
-      </Box>
-
-      <Text>You:</Text>
-      <Box marginX={1}>
-        <SimpleUser user={me} />
-        <Button char="m" onClick={() => { }} />
-      </Box>
-
-      <Box paddingRight={1} backgroundColor={notify_unreads === 0 ? undefined : "red"}>
-        <Text>🔔{notify_unreads}</Text>
-      </Box>
-      <Button char="n" onClick={() => { }} />
-    </Box>
-  );
-}
+const routes = {
+  Home,
+} as const;
 
 export function App() {
+  const [path, navigate] = useState<keyof typeof routes>("Home");
+  const Page = routes[path];
+
   return (
-    <Suspense fallback={
-      <Text>
-        Loading...
-      </Text>
-    }>
-      <AppAsync />
-    </Suspense>
+    <NavigatorContext value={navigate}>
+      <Suspense fallback={
+        <Text>
+          Loading...
+        </Text>
+      }>
+        <Page />
+      </Suspense>
+    </NavigatorContext>
   );
 }
---
+++ src/pages/Home.tsx
@@ -0,0 +1,40 @@
+import { Box, Text } from "ink";
+import { use, useEffect, useState } from "react";
+import { client, realtime } from "../client";
+import type { User } from "../client/types";
+import { SimpleUser } from "../view/SimpleUser";
+import { Button } from "../view/Button";
+
+const me_promise = client.getMe();
+
+export function Home() {
+  const me = use(me_promise).user as User;
+  const [notify_unreads, setNotifyUnreads] = useState(me.notification_unread_count);
+
+  useEffect(() => {
+    realtime.on("notificationUnreadCount", setNotifyUnreads);
+    return () => {
+      realtime.off("notificationUnreadCount", setNotifyUnreads);
+    };
+  }, []);
+
+  return (
+    <Box>
+      <Box marginRight={1}>
+        <Text bold italic>nyaitter-cli</Text>
+        <Button char="h" onClick={() => { }} />
+      </Box>
+
+      <Text>You:</Text>
+      <Box marginX={1}>
+        <SimpleUser user={me} />
+        <Button char="m" onClick={() => { }} />
+      </Box>
+
+      <Box paddingRight={1} backgroundColor={notify_unreads === 0 ? undefined : "red"}>
+        <Text>🔔{notify_unreads}</Text>
+      </Box>
+      <Button char="n" onClick={() => { }} />
+    </Box>
+  );
+}
